8e1625ad2c3b668dcae09ed6fac194c66c8a0064
ryuwc <wom05169@gmail.com>
2022-10-04 21:20:14 +0900

    데이터 삽입, 수정, 삭제 실습

--- day 1/practice/DML.sql
+++ day 1/practice/DML.sql
@@ -122,3 +122,46 @@ WHERE age >= 30;
 SELECT last_name, count(*) AS number_of_name FROM users
 GROUP BY last_name;
 
+-- create
+CREATE TABLE classmates (
+  name TEXT NOT NULL,
+  age INTEGER NOT NULL,
+  address TEXT NOT NULL
+);
+
+-- 단일 행 삽입하기
+INSERT INTO classmates
+VALUES ('홍길동', 23, '서울');
+
+-- 여러 행 삽입하기
+INSERT INTO classmates
+VALUES 
+  ('박씨', 13, '서울'),
+  ('김씨', 22, '대전');
+
+INSERT INTO classmates
+VALUES 
+  ('김철수', 23, '경기'),
+  ('이영미', 22, '강원'),
+  ('박진성', 26, '전라'),
+  ('최지수', 12, '충청'),
+  ('정요한', 28, '경상');
+
+-- 2번 데이터의 이름을 '김철수한무두루미', 주소를 '제주도'로 수정하기
+UPDATE classmates
+SET name='김철수한무두루미',
+    address='제주도'
+WHERE rowid = 2;
+
+-- 5번 데이터 삭제하기
+DELETE FROM classmates
+WHERE rowid = 5;
+
+SELECT rowid, * FROM classmates;
+
+-- 이름에 '영'이 포함되는 데이터 삭제하기
+DELETE FROM classmates
+WHERE name LIKE '%영%';
+
+-- 테이블의 모든 데이터 삭제하기
+DELETE FROM classmates;
